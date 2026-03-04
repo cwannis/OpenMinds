ffdee2f59dbe3ce4885483308be25be23a339cd7
wannis <wannis1710@gmail.com>
2026-03-04 23:52:18 +0100

    maj de api

--- phpFile/getBadgesOfUser.php
+++ phpFile/getBadgesOfUser.php
@@ -4,5 +4,5 @@ header('Content-Type: application/json');
 if(isset($_GET['id'])) $id = $_GET['id'];
 
 if(!empty($id)){
-    echo getBadgesForUser($id, $bdd);
+    echo json_encode(getBadgesForUser($id, $bdd));
 }
